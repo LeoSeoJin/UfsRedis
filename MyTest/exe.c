7a4f7abec1d111e0a1f7431c1643390bfb148ca9
xuejiang <xuejiang1225@gmail.com>
2018-11-20 13:43:14 +0800

    更新测试记录

--- MyTest/exe.c
+++ MyTest/exe.c
@@ -15,29 +15,41 @@
 void replaceChar(char *string, char oldChar, char newChar);
 void *p1(void *arg);
 void *p2(void *arg);
+void *p3(void *arg);
+void *p4(void *arg);
+void *p5(void *arg);
+void *p6(void *arg);
+void *p7(void *arg);
 
-
-char *ip[] = {"127.0.0.1","127.0.0.1","127.0.0.1"};
-char *port[] = {"6379","6380","6381"};
-void *(*p[])(void*) = {p1,p2};  
+char *ip[] = {"127.0.0.1","127.0.0.1","127.0.0.1","127.0.0.1","127.0.0.1","127.0.0.1","127.0.0.1"};
+char *port[] = {"6379","6380","6381","6382","6383","6384","6385","6386"};
+void *(*p[])(void*) = {p1,p2,p3,p4,p5,p6,p7};  
 
 int total_op = 0;
 int union_op = 0;
 int split_op = 0;
+
+char *initial_content[] = {"22,30/47,80/225,890/12,34","12,37,78/257,789,120/36/240,55,556/90,2"};
+int ufs = 0;
+
+char *key_list[] = {"group1","group2","group3","group4"};
+int key = 0;
 
 /*arguments:
  *argv[1]: thread_num
  *argv[2]: total command number (union and split)
  *argv[3]: union command number
  *argv[4]: split command number 
- *argv[5]: initial content of ufs
+ *argv[5]: id of initial content
+ *argv[6]: key of ufs
 */
 
-int main(int argc, char*argv[]) {
+int main(int argc, char*argv[]) {   
     int thread_num = atoi(argv[1]);
     total_op = atoi(argv[2]);
     union_op = atoi(argv[3]);
     split_op = atoi(argv[4]);
+    key = atoi(argv[6])-1;
         
 	pthread_t thread[thread_num];
 	int rc[thread_num];
@@ -52,9 +64,12 @@ int main(int argc, char*argv[]) {
 	//reply = (redisReply*)redisCommand(conn_server,"auth jx062325");
 	//freeReplyObject(reply);
 	
+	ufs = atoi(argv[5])-1;
 	char command[100] = "uinit ";
-	strcat(command,"group1 ");
-	strcat(command,argv[5]);
+	strcat(command,key_list[key]);
+	strcat(command," ");
+	strcat(command,initial_content[ufs]);
+	//printf("ufs: %s\n",initial_content[ufs]);
 	reply = (redisReply*)redisCommand(conn_server,command);
 	
 	freeReplyObject(reply);
@@ -123,16 +138,16 @@ void *p1(void *arg) {
 	if(conn_client->err) printf("Connection error: %s \n",conn_client->errstr);
 
 	redisReply* reply = NULL;	
+	/***
 	reply = (redisReply*)redisCommand(conn_client,"umembers group1");
 	
 	char str[strlen(reply->str)];
 	strcpy(str,reply->str);
-
+    **/
 	
-	//char str[MAX];
-	//strcpy(str,(char*)argv);
-	
-	
+	char str[MAX];
+	strcpy(str,initial_content[ufs]);
+		
     replaceChar(str,'/',',');
 
 	for (i = 0; i < strlen(str); i++)
@@ -151,7 +166,7 @@ void *p1(void *arg) {
 		i++;
 	}
     
-	freeReplyObject(reply);
+	//freeReplyObject(reply);
 	
 	//*generate and execute random operations
 	i = 0;
@@ -183,7 +198,8 @@ void *p1(void *arg) {
 			strcat(command,ufs[argv1]);
 			strcat(command," ");
 			strcat(command,ufs[argv2]);
-			strcat(command," group1");
+			strcat(command," ");
+			strcat(command,key_list[key]);
 				
 			union_num++;	
 		} else {
@@ -191,7 +207,8 @@ void *p1(void *arg) {
 			
 			strcpy(command,"split ");
 			strcat(command,ufs[argv1]);
-			strcat(command," group1");
+			strcat(command," ");
+			strcat(command,key_list[key]);
 			
 			split_num++;			
 		}
@@ -230,21 +247,21 @@ void *p2(void *arg) {
     free((char*)arg);
     
     printf("ip: %s port: %s \n",ip,port);
-   
+    
 	redisContext *conn_client = redisConnect(ip,atoi(port));
 	if(conn_client->err) printf("Connection error: %s \n",conn_client->errstr);
 
 	redisReply* reply = NULL;	
+	/***
 	reply = (redisReply*)redisCommand(conn_client,"umembers group1");
 	
 	char str[strlen(reply->str)];
 	strcpy(str,reply->str);
-
+    **/
 	
-	//char str[MAX];
-	//strcpy(str,(char*)argv);
-	
-	
+	char str[MAX];
+	strcpy(str,initial_content[ufs]);
+		
     replaceChar(str,'/',',');
 
 	for (i = 0; i < strlen(str); i++)
@@ -263,11 +280,12 @@ void *p2(void *arg) {
 		i++;
 	}
     
-	freeReplyObject(reply);
+	//freeReplyObject(reply);
 	
 	//*generate and execute random operations
 	i = 0;
 	int op_type,argv1,argv2;
+
 	int union_num = 0;
 	int split_num = 0;
 	int op_num = total_op;
@@ -295,7 +313,8 @@ void *p2(void *arg) {
 			strcat(command,ufs[argv1]);
 			strcat(command," ");
 			strcat(command,ufs[argv2]);
-			strcat(command," group1");
+			strcat(command," ");
+			strcat(command,key_list[key]);
 				
 			union_num++;	
 		} else {
@@ -303,7 +322,8 @@ void *p2(void *arg) {
 			
 			strcpy(command,"split ");
 			strcat(command,ufs[argv1]);
-			strcat(command," group1");
+			strcat(command," ");
+			strcat(command,key_list[key]);
 			
 			split_num++;			
 		}
@@ -322,3 +342,592 @@ void *p2(void *arg) {
 	return NULL;
 }
 
+void *p3(void *arg) {
+    char *r,*ip,*port;
+    int i = 0;
+    int len = 1;
+    
+    r = strtok((char*)arg,"_");
+    while (r != NULL) {
+       if (i == 0) {
+          ip = (char*)malloc(strlen(r)+1);
+          strcpy(ip,r);
+       } else {
+          port = (char*)malloc(strlen(r)+1);
+          strcpy(port,r);
+       }
+       r = strtok(NULL,"_");
+       i++;
+    }
+    free((char*)arg);
+    
+    printf("ip: %s port: %s \n",ip,port);
+    
+	redisContext *conn_client = redisConnect(ip,atoi(port));
+	if(conn_client->err) printf("Connection error: %s \n",conn_client->errstr);
+
+	redisReply* reply = NULL;	
+	/***
+	reply = (redisReply*)redisCommand(conn_client,"umembers group1");
+	
+	char str[strlen(reply->str)];
+	strcpy(str,reply->str);
+    **/
+	
+	char str[MAX];
+	strcpy(str,initial_content[ufs]);
+		
+    replaceChar(str,'/',',');
+
+	for (i = 0; i < strlen(str); i++)
+		if (str[i] == ',') len++;
+			
+	char *ufs[len];
+	memset(ufs,0,len);
+	for (i = 0; i < len; i++)
+		ufs[i] = (char *)malloc(sizeof(char *));
+		
+	i = 0;
+	r = strtok(str,",");
+	while (r != NULL) {
+		strcpy((char *)ufs[i],(char *)r);
+		r = strtok(NULL,",");
+		i++;
+	}
+    
+	//freeReplyObject(reply);
+	
+
+	//*generate and execute random operations
+
+	i = 0;
+
+	int op_type,argv1,argv2;
+
+	int union_num = 0;
+	int split_num = 0;
+	int op_num = total_op;
+	char *command = NULL;
+	
+	srand((unsigned int)time(NULL));
+	while (i < op_num) {
+	    command = (char *)malloc(sizeof(char)*100);
+
+		//choose operation type:
+		op_type = rand()/(double)RAND_MAX*2;
+		
+		if (union_op != 0 && split_op != 0) {
+			if (op_type == OP_UNION && union_num == union_op) 
+				op_type = OP_SPLIT;
+			if (op_type == OP_SPLIT && split_num == split_op)
+				op_type = OP_UNION;
+		}
+			
+		if (op_type == OP_UNION) {
+			argv1 = rand()/(double)RAND_MAX*len;
+			argv2 = rand()/(double)RAND_MAX*len;
+			
+			strcpy(command,"union ");
+			strcat(command,ufs[argv1]);
+			strcat(command," ");
+			strcat(command,ufs[argv2]);
+			strcat(command," ");
+			strcat(command,key_list[key]);
+				
+			union_num++;	
+		} else {
+			argv1 = rand()/(double)RAND_MAX*len;
+			
+			strcpy(command,"split ");
+			strcat(command,ufs[argv1]);
+			strcat(command," ");
+			strcat(command,key_list[key]);
+			
+			split_num++;			
+		}
+		reply = (redisReply*)redisCommand(conn_client,command);
+		free(command);
+		usleep(50000); //wait 50ms to guarantee send repl ack to server 
+		freeReplyObject(reply);	
+		i++;
+	}
+	
+	redisFree(conn_client);
+	for (i = 0; i < len; i++) free(ufs[i]);
+	
+	free(ip);
+	free(port);
+	return NULL;
+}
+
+void *p4(void *arg) {
+    char *r,*ip,*port;
+    int i = 0;
+    int len = 1;
+    
+    r = strtok((char*)arg,"_");
+    while (r != NULL) {
+       if (i == 0) {
+          ip = (char*)malloc(strlen(r)+1);
+          strcpy(ip,r);
+       } else {
+          port = (char*)malloc(strlen(r)+1);
+          strcpy(port,r);
+       }
+       r = strtok(NULL,"_");
+       i++;
+    }
+    free((char*)arg);
+    
+    printf("ip: %s port: %s \n",ip,port);
+    
+	redisContext *conn_client = redisConnect(ip,atoi(port));
+	if(conn_client->err) printf("Connection error: %s \n",conn_client->errstr);
+
+	redisReply* reply = NULL;	
+	/***
+	reply = (redisReply*)redisCommand(conn_client,"umembers group1");
+	
+	char str[strlen(reply->str)];
+	strcpy(str,reply->str);
+    **/
+	
+	char str[MAX];
+	strcpy(str,initial_content[ufs]);
+		
+    replaceChar(str,'/',',');
+
+	for (i = 0; i < strlen(str); i++)
+		if (str[i] == ',') len++;
+			
+	char *ufs[len];
+	memset(ufs,0,len);
+	for (i = 0; i < len; i++)
+		ufs[i] = (char *)malloc(sizeof(char *));
+		
+	i = 0;
+	r = strtok(str,",");
+	while (r != NULL) {
+		strcpy((char *)ufs[i],(char *)r);
+		r = strtok(NULL,",");
+		i++;
+	}
+    
+	//freeReplyObject(reply);
+	
+
+	//*generate and execute random operations
+
+	i = 0;
+
+	int op_type,argv1,argv2;
+
+	int union_num = 0;
+	int split_num = 0;
+	int op_num = total_op;
+	char *command = NULL;
+	
+	srand((unsigned int)time(NULL));
+	while (i < op_num) {
+	    command = (char *)malloc(sizeof(char)*100);
+
+		//choose operation type:
+		op_type = rand()/(double)RAND_MAX*2;
+		
+		if (union_op != 0 && split_op != 0) {
+			if (op_type == OP_UNION && union_num == union_op) 
+				op_type = OP_SPLIT;
+			if (op_type == OP_SPLIT && split_num == split_op)
+				op_type = OP_UNION;
+		}
+			
+		if (op_type == OP_UNION) {
+			argv1 = rand()/(double)RAND_MAX*len;
+			argv2 = rand()/(double)RAND_MAX*len;
+			
+			strcpy(command,"union ");
+			strcat(command,ufs[argv1]);
+			strcat(command," ");
+			strcat(command,ufs[argv2]);
+			strcat(command," ");
+			strcat(command,key_list[key]);
+				
+			union_num++;	
+		} else {
+			argv1 = rand()/(double)RAND_MAX*len;
+			
+			strcpy(command,"split ");
+			strcat(command,ufs[argv1]);
+			strcat(command," ");
+			strcat(command,key_list[key]);
+			
+			split_num++;			
+		}
+		reply = (redisReply*)redisCommand(conn_client,command);
+		free(command);
+		usleep(50000); //wait 50ms to guarantee send repl ack to server 
+		freeReplyObject(reply);	
+		i++;
+	}
+	
+	redisFree(conn_client);
+	for (i = 0; i < len; i++) free(ufs[i]);
+	
+	free(ip);
+	free(port);
+	return NULL;
+}
+
+void *p5(void *arg) {
+    char *r,*ip,*port;
+    int i = 0;
+    int len = 1;
+    
+    r = strtok((char*)arg,"_");
+    while (r != NULL) {
+       if (i == 0) {
+          ip = (char*)malloc(strlen(r)+1);
+          strcpy(ip,r);
+       } else {
+          port = (char*)malloc(strlen(r)+1);
+          strcpy(port,r);
+       }
+       r = strtok(NULL,"_");
+       i++;
+    }
+    free((char*)arg);
+    
+    printf("ip: %s port: %s \n",ip,port);
+    
+	redisContext *conn_client = redisConnect(ip,atoi(port));
+	if(conn_client->err) printf("Connection error: %s \n",conn_client->errstr);
+
+	redisReply* reply = NULL;	
+	/***
+	reply = (redisReply*)redisCommand(conn_client,"umembers group1");
+	
+	char str[strlen(reply->str)];
+	strcpy(str,reply->str);
+    **/
+	
+	char str[MAX];
+	strcpy(str,initial_content[ufs]);
+		
+    replaceChar(str,'/',',');
+
+	for (i = 0; i < strlen(str); i++)
+		if (str[i] == ',') len++;
+			
+	char *ufs[len];
+	memset(ufs,0,len);
+	for (i = 0; i < len; i++)
+		ufs[i] = (char *)malloc(sizeof(char *));
+		
+	i = 0;
+	r = strtok(str,",");
+	while (r != NULL) {
+		strcpy((char *)ufs[i],(char *)r);
+		r = strtok(NULL,",");
+		i++;
+	}
+    
+	//freeReplyObject(reply);
+	
+
+	//*generate and execute random operations
+
+	i = 0;
+
+	int op_type,argv1,argv2;
+
+	int union_num = 0;
+	int split_num = 0;
+	int op_num = total_op;
+	char *command = NULL;
+	
+	srand((unsigned int)time(NULL));
+	while (i < op_num) {
+	    command = (char *)malloc(sizeof(char)*100);
+
+		//choose operation type:
+		op_type = rand()/(double)RAND_MAX*2;
+		
+		if (union_op != 0 && split_op != 0) {
+			if (op_type == OP_UNION && union_num == union_op) 
+				op_type = OP_SPLIT;
+			if (op_type == OP_SPLIT && split_num == split_op)
+				op_type = OP_UNION;
+		}
+			
+		if (op_type == OP_UNION) {
+			argv1 = rand()/(double)RAND_MAX*len;
+			argv2 = rand()/(double)RAND_MAX*len;
+			
+			strcpy(command,"union ");
+			strcat(command,ufs[argv1]);
+			strcat(command," ");
+			strcat(command,ufs[argv2]);
+			strcat(command," ");
+			strcat(command,key_list[key]);
+				
+			union_num++;	
+		} else {
+			argv1 = rand()/(double)RAND_MAX*len;
+			
+			strcpy(command,"split ");
+			strcat(command,ufs[argv1]);
+			strcat(command," ");
+			strcat(command,key_list[key]);
+			
+			split_num++;			
+		}
+		reply = (redisReply*)redisCommand(conn_client,command);
+		free(command);
+		usleep(50000); //wait 50ms to guarantee send repl ack to server 
+		freeReplyObject(reply);	
+		i++;
+	}
+	
+	redisFree(conn_client);
+	for (i = 0; i < len; i++) free(ufs[i]);
+	
+	free(ip);
+	free(port);
+	return NULL;
+}
+
+void *p6(void *arg) {
+    char *r,*ip,*port;
+    int i = 0;
+    int len = 1;
+    
+    r = strtok((char*)arg,"_");
+    while (r != NULL) {
+       if (i == 0) {
+          ip = (char*)malloc(strlen(r)+1);
+          strcpy(ip,r);
+       } else {
+          port = (char*)malloc(strlen(r)+1);
+          strcpy(port,r);
+       }
+       r = strtok(NULL,"_");
+       i++;
+    }
+    free((char*)arg);
+    
+    printf("ip: %s port: %s \n",ip,port);
+    
+	redisContext *conn_client = redisConnect(ip,atoi(port));
+	if(conn_client->err) printf("Connection error: %s \n",conn_client->errstr);
+
+	redisReply* reply = NULL;	
+	/***
+	reply = (redisReply*)redisCommand(conn_client,"umembers group1");
+	
+	char str[strlen(reply->str)];
+	strcpy(str,reply->str);
+    **/
+	
+	char str[MAX];
+	strcpy(str,initial_content[ufs]);
+		
+    replaceChar(str,'/',',');
+
+	for (i = 0; i < strlen(str); i++)
+		if (str[i] == ',') len++;
+			
+	char *ufs[len];
+	memset(ufs,0,len);
+	for (i = 0; i < len; i++)
+		ufs[i] = (char *)malloc(sizeof(char *));
+		
+	i = 0;
+	r = strtok(str,",");
+	while (r != NULL) {
+		strcpy((char *)ufs[i],(char *)r);
+		r = strtok(NULL,",");
+		i++;
+	}
+    
+	//freeReplyObject(reply);
+	
+
+	//*generate and execute random operations
+
+	i = 0;
+
+	int op_type,argv1,argv2;
+
+	int union_num = 0;
+	int split_num = 0;
+	int op_num = total_op;
+	char *command = NULL;
+	
+	srand((unsigned int)time(NULL));
+	while (i < op_num) {
+	    command = (char *)malloc(sizeof(char)*100);
+
+		//choose operation type:
+		op_type = rand()/(double)RAND_MAX*2;
+		
+		if (union_op != 0 && split_op != 0) {
+			if (op_type == OP_UNION && union_num == union_op) 
+				op_type = OP_SPLIT;
+			if (op_type == OP_SPLIT && split_num == split_op)
+				op_type = OP_UNION;
+		}
+			
+		if (op_type == OP_UNION) {
+			argv1 = rand()/(double)RAND_MAX*len;
+			argv2 = rand()/(double)RAND_MAX*len;
+			
+			strcpy(command,"union ");
+			strcat(command,ufs[argv1]);
+			strcat(command," ");
+			strcat(command,ufs[argv2]);
+			strcat(command," ");
+			strcat(command,key_list[key]);
+				
+			union_num++;	
+		} else {
+			argv1 = rand()/(double)RAND_MAX*len;
+			
+			strcpy(command,"split ");
+			strcat(command,ufs[argv1]);
+			strcat(command," ");
+			strcat(command,key_list[key]);
+			
+			split_num++;			
+		}
+		reply = (redisReply*)redisCommand(conn_client,command);
+		free(command);
+		usleep(50000); //wait 50ms to guarantee send repl ack to server 
+		freeReplyObject(reply);	
+		i++;
+	}
+	
+	redisFree(conn_client);
+	for (i = 0; i < len; i++) free(ufs[i]);
+	
+	free(ip);
+	free(port);
+	return NULL;
+}
+
+void *p7(void *arg) {
+    char *r,*ip,*port;
+    int i = 0;
+    int len = 1;
+    
+    r = strtok((char*)arg,"_");
+    while (r != NULL) {
+       if (i == 0) {
+          ip = (char*)malloc(strlen(r)+1);
+          strcpy(ip,r);
+       } else {
+          port = (char*)malloc(strlen(r)+1);
+          strcpy(port,r);
+       }
+       r = strtok(NULL,"_");
+       i++;
+    }
+    free((char*)arg);
+    
+    printf("ip: %s port: %s \n",ip,port);
+    
+	redisContext *conn_client = redisConnect(ip,atoi(port));
+	if(conn_client->err) printf("Connection error: %s \n",conn_client->errstr);
+
+	redisReply* reply = NULL;	
+	/***
+	reply = (redisReply*)redisCommand(conn_client,"umembers group1");
+	
+	char str[strlen(reply->str)];
+	strcpy(str,reply->str);
+    **/
+	
+	char str[MAX];
+	strcpy(str,initial_content[ufs]);
+		
+    replaceChar(str,'/',',');
+
+	for (i = 0; i < strlen(str); i++)
+		if (str[i] == ',') len++;
+			
+	char *ufs[len];
+	memset(ufs,0,len);
+	for (i = 0; i < len; i++)
+		ufs[i] = (char *)malloc(sizeof(char *));
+		
+	i = 0;
+	r = strtok(str,",");
+	while (r != NULL) {
+		strcpy((char *)ufs[i],(char *)r);
+		r = strtok(NULL,",");
+		i++;
+	}
+    
+	//freeReplyObject(reply);
+	
+
+	//*generate and execute random operations
+
+	i = 0;
+
+	int op_type,argv1,argv2;
+
+	int union_num = 0;
+	int split_num = 0;
+	int op_num = total_op;
+	char *command = NULL;
+	
+	srand((unsigned int)time(NULL));
+	while (i < op_num) {
+	    command = (char *)malloc(sizeof(char)*100);
+
+		//choose operation type:
+		op_type = rand()/(double)RAND_MAX*2;
+		
+		if (union_op != 0 && split_op != 0) {
+			if (op_type == OP_UNION && union_num == union_op) 
+				op_type = OP_SPLIT;
+			if (op_type == OP_SPLIT && split_num == split_op)
+				op_type = OP_UNION;
+		}
+			
+		if (op_type == OP_UNION) {
+			argv1 = rand()/(double)RAND_MAX*len;
+			argv2 = rand()/(double)RAND_MAX*len;
+			
+			strcpy(command,"union ");
+			strcat(command,ufs[argv1]);
+			strcat(command," ");
+			strcat(command,ufs[argv2]);
+			strcat(command," ");
+			strcat(command,key_list[key]);
+				
+			union_num++;	
+		} else {
+			argv1 = rand()/(double)RAND_MAX*len;
+			
+			strcpy(command,"split ");
+			strcat(command,ufs[argv1]);
+			strcat(command," ");
+			strcat(command,key_list[key]);
+			
+			split_num++;			
+		}
+		reply = (redisReply*)redisCommand(conn_client,command);
+		free(command);
+		usleep(50000); //wait 50ms to guarantee send repl ack to server 
+		freeReplyObject(reply);	
+		i++;
+	}
+	
+	redisFree(conn_client);
+	for (i = 0; i < len; i++) free(ufs[i]);
+	
+	free(ip);
+	free(port);
+	return NULL;
+}
